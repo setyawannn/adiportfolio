3d3cb7824449247352069c6a335e3bb987b12041
setyawannn <prayogasetyawan.adi@gmail.com>
2023-03-03 11:33:41 +0700

    add: detail-product

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,12 +1,94 @@
 "use client";
-import React from "react";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { GoPlus, GoDash, GoTrashcan } from "react-icons/go";
 
 export default function page({ params }: { params: { slug: number } }) {
   const id = Number(params.slug);
+  const [count, setCount] = useState<number>(1);
+  const price = 82000;
+
+  const ToIDR = price.toLocaleString("id-ID", {
+    currency: "IDR",
+  });
+
+  const handleMinus = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else if (count === 1) {
+      setCount(1);
+    }
+  };
+
+  const handlePlus = () => {
+    if (count < 5) {
+      setCount(count + 1);
+    } else if (count === 5) {
+      setCount(5);
+    }
+  };
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      id barang : {id}
-    </div>
+    <section>
+      <div className="container relative top-40 max-w-[1200px] m-auto">
+        <div className="flex gap-10">
+          <div className="w-1/5">
+            <Image
+              src={"/assets/images/allaboutmindset.png"}
+              alt="mindset"
+              width={400}
+              height={400}
+              className="w-full h-fit"
+            />
+          </div>
+          <div className="w-3/5 py-4">
+            <h3 className="text-4xl font-semibold">Seni Mencintai Batu</h3>
+            <h5 className="text-lg font-medium mt-2">Fiksi, Novel</h5>
+            <div className="w-52 p-4 bg-zinc-100 mt-6 border border-secondary rounded cursor-pointer hover:bg-zinc-300 duration-100">
+              <span className="text-base font-medium">SOFT COVER</span>
+              <p className="text-xl text-secondary font-medium">IDR 82.000</p>
+            </div>
+            <div className="mt-10">
+              <h4 className="text-xl font-bold">Deskripsi Buku</h4>
+              <p className="text-lg mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique, quo error? Labore, numquam! Recusandae veritatis eius
+                cupiditate autem odit soluta doloremque et ipsam, vel sed,
+                itaque expedita possimus sapiente quae totam facere! Modi
+                praesentium aut perspiciatis quia sit magni esse. Quibusdam
+                laudantium libero assumenda fugiat veniam ratione architecto
+                corrupti natus!
+              </p>
+            </div>
+          </div>
+          <div className="w-1/5 bg-zinc-100 p-5 rounded-md border border-secondary">
+            <div>
+              <h5 className="text-lg font-semibold text-zinc-700">
+                Ingin beli berapa?
+              </h5>
+              <h6 className="text-base font-medium text-secondary">
+                Jumlah Barang
+              </h6>
+            </div>
+            <div className="flex mt-4 gap-4 items-center">
+              <GoDash
+                onClick={handleMinus}
+                className="text-xl cursor-pointer"
+              />
+              <h6 className="text-xl font-bold">{count}</h6>
+              <GoPlus onClick={handlePlus} className="text-xl cursor-pointer" />
+              <GoTrashcan onClick={() => setCount(1)} />
+            </div>
+            <div className="h-[1px] w-full bg-secondary my-8"></div>
+            <div className="flex justify-between">
+              <h5 className="text-base font-bold text-zinc-500">Subtotal</h5>
+              <h6 className="text-base font-bold text-secondary">
+                IDR {price * count}
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
